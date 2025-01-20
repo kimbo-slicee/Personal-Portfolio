44bd1c6280cc14de41997ea763ced707a872796f
mohammed el aouri <mohammedelaouri@gmail.com>
2025-01-21 00:41:32 +0100

    Commit Message:`Create PortFolio Section`

--- Portfolio-02/assets/js/main.js
+++ Portfolio-02/assets/js/main.js
@@ -27,14 +27,100 @@ const activateTab = (index) => {
 /* =====================================================
    Service modal open/close function
 ===================================================== */
+const serviceCardWithModals=document.querySelectorAll(".service-container .card-with-modal");
+serviceCardWithModals.forEach((serviceCardWithModal)=>{
+const serviceCard=serviceCardWithModal.querySelector(".service-card");
+const serviceBackDrop=serviceCardWithModal.querySelector(".service-modal-backdrop");
+const serviceModel=serviceCardWithModal.querySelector(".service-modal");
+const modalCloseBtn=serviceCardWithModal.querySelector(".modal-close-btn");
+console.log(modalCloseBtn)
+serviceCard.addEventListener("click",()=>{
+    serviceBackDrop.style.display="flex";
+    setTimeout(()=>{
+        serviceBackDrop.classList.add("active")
+    },100)
+    setTimeout(()=>{
+        serviceModel.classList.add("active")
+    },300)
+})
+modalCloseBtn.addEventListener("click",()=>{
+    setTimeout(()=>{
+    serviceBackDrop.style.display="none";
+    },500)
+    setTimeout(()=>{
+    serviceBackDrop.classList.remove("active");
+    serviceModel.classList.remove("active")
+    },100)
+})
+})
 
 /* =====================================================
    Portfolio modals, tabs and cards
 ===================================================== */
+document.addEventListener("DOMContentLoaded",()=>{
+    const portfolioTabs=document.querySelector(".portfolio-tabs");
+    const portfolioTabBtns=portfolioTabs.querySelectorAll(".tab-btn");
+    console.log(portfolioTabBtns)
+    const cardsWithModals=document.querySelectorAll(".portfolio-container .card-with-modal");
+    portfolioTabBtns.forEach((tabBtn)=>{
+        tabBtn.addEventListener("click",()=>{
+            const filter=tabBtn.getAttribute("data-filter");
+            cardsWithModals.forEach((cardsWithModal)=>{
+                if(filter==="all" || cardsWithModal.classList.contains(filter)){
+                    cardsWithModal.classList.remove("hidden")
+                    setTimeout(()=>{
+                    cardsWithModal.style.opacity="1"
+                    cardsWithModal.style.transition=".5s ease"
+
+                    },1)
+                }else {
+                    cardsWithModal.style.opacity="0";
+                    cardsWithModal.classList.add("hidden")
+                }
+            })
+            // Add Active class to the clicked tab button
+            portfolioTabBtns.forEach((tabBtn)=>tabBtn.classList.remove("active"))
+            tabBtn.classList.add("active")
+
+        })
+    })
+})
+
+
+
+
+
 
 // Filter portfolio cards according to portfolio tabs.
 
 // Open/Close Portfolio modals.
+const portfolioCardWithModals=document.querySelectorAll(".portfolio-container .card-with-modal");
+portfolioCardWithModals.forEach(portfolioCardWithModal=>{
+    const portfolioCard=portfolioCardWithModal.querySelector(".portfolio-card");
+    const portfolioBackdrop=portfolioCardWithModal.querySelector(".portfolio-modal-backdrop");
+    const portfolioModal=portfolioCardWithModal.querySelector(".portfolio-modal");
+    const modalCloseBtn=portfolioCardWithModal.querySelector(".modal-close-btnx");
+    portfolioCard.addEventListener("click",()=>{
+        portfolioBackdrop.style.display="flex";
+        setTimeout(()=>{
+        portfolioBackdrop.classList.add("active");
+            },300)
+        setTimeout(()=>{
+        portfolioModal.classList.add("active")
+
+        },300)
+    })
+    modalCloseBtn.addEventListener("click",()=>{
+        setTimeout(()=>{
+            portfolioBackdrop.style.display="none";
+        },300)
+        setTimeout(()=>{
+            portfolioBackdrop.classList.remove("active");
+            portfolioModal.classList.remove("active")
+
+        },300)
+    })
+})
 
 /* =====================================================
    Testimonial Swiper
