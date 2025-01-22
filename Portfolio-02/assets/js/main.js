09a59728e791f4cf7124ca2c3c829d7eb9bb9b00
mohammed el aouri <mohammedelaouri@gmail.com>
2025-01-22 23:24:29 +0100

    Commit message: `Bottom navigation menu behavior - on page load, page scroll, click menu show/hide buttons (Html, Css, Javascript)`

--- Portfolio-02/assets/js/main.js
+++ Portfolio-02/assets/js/main.js
@@ -160,18 +160,66 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 /* =====================================================
    Shrink the height of the header on scroll
 ===================================================== */
+window.addEventListener("scroll",()=>{
+const  header=document.querySelector(".header");
+header.classList.toggle("shrink",window.scrollY>0)
 
+})
 /* =====================================================
    Bottom navigation menu
 ===================================================== */
 
 // Each bottom navigation menu items active on page scroll.
+window.addEventListener("scroll",()=>{
+    const NavMeshSections=document.querySelectorAll(".nav-menu-section");
+    const scrollY=window.pageYOffset
+    NavMeshSections.forEach(navMenuSection=>{
+        let sectionHeight=navMenuSection.offsetHeight;
+        let sectionTop=navMenuSection.offsetTop-50
+        let id =navMenuSection.getAttribute("id")
+        if(scrollY>sectionTop && scrollY<=sectionTop + sectionHeight){
+            document.querySelector(".bottom-nav .menu li a[href*="+id+"]").classList.add("current")
+        }else {
+            document.querySelector(".bottom-nav .menu li a[href*="+id+"]").classList.remove("current")
+        }
+    })
+})
 
 // Javascript to show bottom navigation menu on home(page load).
+window.addEventListener("DOMContentLoaded",()=>{
+    const bottomNav=document.querySelector(".bottom-nav");
+    bottomNav.classList.toggle("active",window.scrollY<10)
+})
 
 // Javascript to show/hide bottom navigation menu on home(scroll).
+const bottomNav=document.querySelector(".bottom-nav");
+const menuHideBtn=document.querySelector(".menu-hide-btn");
+const menuShowBtn=document.querySelector(".menu-show-btn");
+let navTimeout;
+window.addEventListener("scroll",()=>{
+    bottomNav.classList.add("active");
+    menuShowBtn.classList.remove("active")
+    if(window.scrollY < 10){
+        menuHideBtn.classList.remove("active")
+        function scrollStopped() {
+            bottomNav.classList.add("active")
+        }
+        clearTimeout(navTimeout);
+        navTimeout=setTimeout(scrollStopped,2500)
+    }
+    if(window.scrollY>10){
+        menuHideBtn.classList.add("active")
+        function scrollStopped() {
+            bottomNav.classList.remove("active")
+            menuShowBtn.classList.add("active")
+        }
+        clearTimeout(navTimeout);
+        navTimeout=setTimeout(scrollStopped,2500)
+    }
+})
 
 // Hide bottom navigation menu on click menu-hide-btn.
+
 
 // Show bottom navigation menu on click menu-show-btn.
 
