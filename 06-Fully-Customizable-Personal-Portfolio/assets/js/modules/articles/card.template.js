345203921f1503152c96049f563926f039be7421
mohammed el aouri <mohammedelaouri@gmail.com>
2026-01-15 20:26:59 +0100

    `Refactoring Articles section`

--- 06-Fully-Customizable-Personal-Portfolio/assets/js/modules/articles/card.template.js
+++ 06-Fully-Customizable-Personal-Portfolio/assets/js/modules/articles/card.template.js
@@ -1,3 +1,10 @@
+const icons={
+    calendar:`
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon icon--sm">
+        <path d="M9 1V3H15V1H17V3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H7V1H9ZM20 8H4V19H20V8ZM15.0355 10.136L16.4497 11.5503L11.5 16.5L7.96447 12.9645L9.37868 11.5503L11.5 13.6716L15.0355 10.136Z"></path></svg>
+    `
+
+}
 import dateFormatter from "../../utilities/dateFormatter.js";
 import {extractMediumPreview} from "../../utilities/htmlExtractor.js";
 
@@ -7,16 +14,43 @@ export function createArticleCard({
                                       description,
                                       link,
                                       categories = [],
-                                      author = "Mohamed",
+                                      author = "Mohamed el aouri",
                                       variant = "vertical" // vertical | horizontal
                                   }) {
     const formattedDate = dateFormatter(new Date(pubDate));
-    const { src, desc } = extractMediumPreview(description, 140);
+    const { src, desc } = extractMediumPreview(description, 200);
+    const categoryTags = categories.slice(0,3)
+        .map(c => {
+            return `<span class="tag">${c}</span>`;
+        })
+        .join("");
 
     return `
-        <article class="article-card">
+            <article class="article-card">
+              <div class="article-card__media">
+                <img src="${src}" alt="${title}">
+              </div>
             
-        </article>
+              <div class="article-card__content">
+                <div class="article-card__meta">
+                  <div class="article-card__time">
+                        <span>${formattedDate}</span>
+                        <span>${icons.calendar}</span>
+                  </div>
+                  <a class="article-card__read" href="${link}" target="_blank">article →</a>   
+                </div>
+            
+                <h3 class="article-card__title">${title}</h3>
+            
+                <p class="article-card__excerpt">${desc}</p>
+            
+                <div class="article-card__footer">
+                   <div class="article-card__categories">
+                        ${categoryTags}
+                   </div>
+                </div>
+              </div>
+            </article>
   `;
 }
 
