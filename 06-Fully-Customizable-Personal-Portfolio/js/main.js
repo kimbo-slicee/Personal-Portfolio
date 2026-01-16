c1e382fb13b36f919b81ca13a50c1906031b016a
mohammed el aouri <mohammedelaouri@gmail.com>
2026-01-16 19:09:55 +0100

    🦅

--- 06-Fully-Customizable-Personal-Portfolio/js/main.js
+++ 06-Fully-Customizable-Personal-Portfolio/js/main.js
@@ -1,7 +1,7 @@
 import "./theme/modes.js";
 import "./features/carousel/carousel.js";
-import "./modules/portfolio";
-import "./modules/articles";
+import "./modules/portfolio/index";
+import "./modules/articles/index";
 import "./modules/resume/resume.js";
 import "./modules/bottomNavBar/bottomNav.js";
 import "./features/cursor.js";
